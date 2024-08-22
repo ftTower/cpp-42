aa6d1f8fb6c01d7f0e46da51f2d8838fc1740e50
ftTower <thomas.auer67@gmail.com>
2024-08-23 01:26:43 +0200

    chore: Refactor Makefile and add Sed class implementation

---
+++ cpp_1/ex04/main.cpp
@@ -0,0 +1,10 @@
+
+#include "sed.hpp"
+
+int main(int argc, char **argv)
+{
+    
+
+
+    return (0);
+}
---
+++ cpp_1/ex04/sed.hpp
@@ -0,0 +1,15 @@
+#ifndef SED_HPP
+# define SED_HPP
+
+#include <iostream>
+
+class FileReplace {
+
+    public:
+    FileReplace(const std::string &filename, const std::string &s1, const std::string &s2);
+    
+
+
+}
+
+#endif
