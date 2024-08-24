b0d17b54d6b9eddf7a3b8e71e6d5cf12dd2af934
ftTower <thomas.auer67@gmail.com>
2024-08-24 18:25:18 +0200

    refactor: Update Harl class to use enum for log levels

--- cpp_1/ex05/Harl.hpp
+++ cpp_1/ex05/Harl.hpp
@@ -4,31 +4,34 @@
 #include <iostream>
 // #include <>
 
+typedef enum e_level{
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    UNKNOW,
+}   t_level;
+
 class Harl {
 
     private:
     void    debug(void) {
-        std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
-    }
+        std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;}
     void    info(void)  {
-        std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
-    }
+        std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;}
     void    warning(void) {
-        std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
-    }
+        std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl; }
     void    error(void) {
-        std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
-    }
+        std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;}
     void    unknow(void) {
-        std::cout << "UNKNOW: Unknow level" << std::endl;
-    }
+        std::cout << "UNKNOW: Unknow level" << std::endl;}
     
-    int getLevelInfo(std::string level){
-        if (level == "DEBUG") {return (0);}
-        else if (level == "INFO") {return (1);}
-        else if (level == "WARNING") {return (2);}
-        else if (level == "ERROR") {return (3);}
-        else {return (4);}
+    t_level getLevelInfo(std::string level){
+        if (level == "DEBUG") {return (DEBUG);}
+        else if (level == "INFO") {return (INFO);}
+        else if (level == "WARNING") {return (WARNING);}
+        else if (level == "ERROR") {return (ERROR);}
+        else {return (UNKNOW);}
     }
     
     public:
