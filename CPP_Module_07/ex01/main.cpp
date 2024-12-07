d9eaa311f6f8d31bac1f1092da3edc27867cba93
Thomas <thomas.auer67@gmail.com>
2024-12-07 02:31:20 +0100

    refactor: rename Array to Iter in relevant files; update main function to include test cases for char and int arrays

--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -6,12 +6,16 @@
 /*   By: tauer <tauer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 19:04:28 by tauer             #+#    #+#             */
-/*   Updated: 2024/11/24 01:57:41 by tauer            ###   ########.fr       */
+/*   Updated: 2024/12/07 02:22:56 by tauer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Iter.hpp"
 // #include "../../CPP_Module_06/ex00/ScalarConverter.hpp"
+
+void ft_plusOne(int &arr) {
+	arr += 1;
+}
 
 void ft_tolower(char &arr)
 {
@@ -23,7 +27,7 @@ void ft_toupper(char &arr)
 	arr = std::toupper(static_cast<unsigned char>(arr));
 }
 
-int main()
+void	test_char()
 {
 	char a[] = {'A', 'B', 'C'};
 
@@ -48,4 +52,33 @@ int main()
 	"\n\ta[1]: " << a[1] <<
 	"\n\ta[2]: " << a[2] <<
 	std::endl << std::endl;
+}
+
+void	test_int()
+{
+	int arr[] = {10, 5, 6};
+
+	std::cout << "Original:" <<
+	"\n\ta[0]: " << arr[0] <<
+	"\n\ta[1]: " << arr[1] <<
+	"\n\ta[2]: " << arr[2] <<
+	std::endl << std::endl;
+
+	::iter(arr, 2, ft_plusOne);
+
+	std::cout << "Changed:" <<
+	"\n\ta[0]: " << arr[0] <<
+	"\n\ta[1]: " << arr[1] <<
+	"\n\ta[2]: " << arr[2] <<
+	std::endl << std::endl;
+
+}
+
+int main()
+{
+	test_char();
+
+	std::cout << "==========================" << std::endl;
+
+	test_int();
 }
