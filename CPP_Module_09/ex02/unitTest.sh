5723775d81ccc5e30ed80b9df1e9db9f4073b79e
ftTower <thomas.auer67@gmail.com>
2025-01-07 01:49:28 +0100

    test: add additional test cases to unitTest.sh for PmergeMe with random input

--- CPP_Module_09/ex02/unitTest.sh
+++ CPP_Module_09/ex02/unitTest.sh
@@ -4,13 +4,17 @@
 PMM_PROGRAM="./PmergeMe"
 LINE="==================================="
 
-make re && clear && echo 
+make re && clear && echo
+
+echo "Test 5 :" && echo 
+$PMM_PROGRAM  `shuf -i 1-5 -n 5 | tr "\n" " "`
 
 # echo "Test 1000 :" && echo 
 # $PMM_PROGRAM  `shuf -i 1-100000 -n 1000 | tr "\n" " "`
 
 # echo "Test 2000 :" && echo 
 # $PMM_PROGRAM  `shuf -i 1-100000 -n 2000 | tr "\n" " "`
+
 
 echo "Test 3000 :" && echo 
 $PMM_PROGRAM  `shuf -i 1-100000 -n 3000 | tr "\n" " "`
